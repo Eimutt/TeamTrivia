24c5f29fc37db7f8f40ffc44e0bfc777bd55d05e
isakpeterson <isakp@kth.se>
2018-05-16 17:51:36 +0200

    made categories dynamic

--- src/GameSetup.js
+++ src/GameSetup.js
@@ -10,7 +10,9 @@ class GameSetup extends React.Component {
     // we put on state the properties we want to use and modify in the component
     this.state = {
       numberOfQuestions: 10,
-      categories: [{"categoryid":15,"categoryname":"Video Games"},{"categoryid":10,"categoryname":"Books"},{"categoryid":31,"categoryname":"Anime and Manga"},{"categoryid":17,"categoryname":"Science and Nature"},{"categoryid":12,"categoryname":"Music"},{"categoryid":11,"categoryname":"Film"},{"categoryid":9,"categoryname":"General Knowledge"},{"categoryid":14,"categoryname":"Television"},{"categoryid":22,"categoryname":"Geography"},{"categoryid":20,"categoryname":"Mythology"},{"categoryid":19,"categoryname":"Mathematics"},{"categoryid":18,"categoryname":"Computers"},{"categoryid":23,"categoryname":"History"},{"categoryid":24,"categoryname":"Politics"},{"categoryid":27,"categoryname":"Animals"},{"categoryid":26,"categoryname":"Celebrities"}],
+      categories: [],
+      currentcategories: [],
+      //categories: [{"categoryid":15,"categoryname":"Video Games"},{"categoryid":10,"categoryname":"Books"},{"categoryid":31,"categoryname":"Anime and Manga"},{"categoryid":17,"categoryname":"Science and Nature"},{"categoryid":12,"categoryname":"Music"},{"categoryid":11,"categoryname":"Film"},{"categoryid":9,"categoryname":"General Knowledge"},{"categoryid":14,"categoryname":"Television"},{"categoryid":22,"categoryname":"Geography"},{"categoryid":20,"categoryname":"Mythology"},{"categoryid":19,"categoryname":"Mathematics"},{"categoryid":18,"categoryname":"Computers"},{"categoryid":23,"categoryname":"History"},{"categoryid":24,"categoryname":"Politics"},{"categoryid":27,"categoryname":"Animals"},{"categoryid":26,"categoryname":"Celebrities"}],
       lobbyID: ""
     }
   }
@@ -71,31 +73,38 @@ class GameSetup extends React.Component {
     })
   }
 
-  addCategory = (categoryid, categoryname) => {
-    var currentcategories = this.state.categories;
-    var category = {categoryid, categoryname};
+  addCategory = (id, name) => {
+    //var currentcategories = this.state.categories;
+    var currcats = this.state.currentcategories;
+    var category = {id, name};
     var found = false;
-    for (var i = 0; i < currentcategories.length; i++) {
-        if (currentcategories[i].categoryid == categoryid) {
-          currentcategories.splice(i,1);
+    for (var i = 0; i < currcats.length; i++) {
+        if (currcats[i].id == id) {
+          currcats.splice(i,1);
           found = true;
           break;
         }
       }
       if (!found)
-        currentcategories.push(category);
+        currcats.push(category);
 
     this.setState({
-      categories: currentcategories,
+      currentcategories: currcats,
     })
-    console.log(JSON.stringify(this.state.categories));
+    //console.log("Current categories: " + JSON.stringify(this.state.currentcategories));
   }
 
   componentDidMount() {
-
+    this.fetchCategories().then((json) => {
+      console.log(json);
+      this.setState({
+        categories: json.trivia_categories,
+      })
+    });
   }
 
   render() {
+    console.log("Current categories: " + JSON.stringify(this.state.currentcategories));
     var toPrintLink = "";
     if(this.state.lobbyID !== "")
       toPrintLink = (
@@ -105,8 +114,9 @@ class GameSetup extends React.Component {
         </div>
       )
     var confirmButton = "";
-    console.log("Number of categories: " + this.state.categories.length);
-    if (this.state.categories.length == 0) {
+    console.log("Number of total categories: " + this.state.categories.length);
+    console.log("Number of current categories: " + this.state.currentcategories.length);
+    if (this.state.currentcategories.length == 0) {
       confirmButton = (
         <div class="btnhandler">
           <button id="startbutton" disabled type="button" class="btn btn-primary btn-lg" onClick={() => this.handleChange()}>Create Lobby</button>
@@ -127,7 +137,7 @@ class GameSetup extends React.Component {
             <div>Number of Questions:</div>
             <input id="numberOfQuestions" type="number" min="1" input="number" value={this.state.numberOfQuestions} onChange={this.onNumberOfGuestsChanged}></input>
           </div>
-          <Categories callback={this.addCategory}/>
+          <Categories categories={this.state.categories} callback={this.addCategory}/>
         </div>
         {confirmButton}
         {toPrintLink}
@@ -151,132 +161,43 @@ class Categories extends React.Component {
   }
 
   render() {
+    var categories = this.props.categories;
+    var columnsize = (categories.length/4);
+    var remainder = (categories.length % 4);
+    var columns = [];
+    for (var i = 0; i < 4; i++) {
+      currentcolumnsize = columnsize;
+      var column = [];
+      if (remainder != 0) {
+        var currentcolumnsize = columnsize + 1;
+        remainder--;
+      }
+      for (var j = 0; j < currentcolumnsize; j++) {
+        column.push(categories[j]);
+      }
+      categories.splice(0,currentcolumnsize);
+      columns.push(column);
+    }
+    console.log(columns);
     return (
       <div className = "CategoryContainer">
         <div className= "categoryTitle">{"Categories"}</div>
         <Grid
           width={0}
           gap={0}>
-          <div id="column">
-            <div className= "griditem">
-              <div className="categoryname">General Knowledge</div>
-              <label class="switch">
-                <input type="checkbox" onClick={() => this.handleClick(9, "General Knowledge")}></input>
-                <span class="slider round"></span>
-              </label>
+          {columns.map((column) =>
+            <div id="column">
+              {column.map((category) =>
+                <div className= "griditem">
+                  <div className="categoryname">{category.name.replace("Entertainment: ", "").replace("Science: ", "")}</div>
+                  <label class="switch">
+                    <input type="checkbox" onClick={() => this.handleClick(category.id, category.name)}></input>
+                    <span class="slider round"></span>
+                  </label>
+                </div>
+              )}
             </div>
-            <div className= "griditem">
-              <div className="categoryname">Film</div>
-              <label class="switch">
-                <input type="checkbox" onClick={() => this.handleClick(11, "Film")}></input>
-                <span class="slider round"></span>
-              </label>
-            </div>
-            <div className= "griditem">
-              <div className="categoryname">Music</div>
-              <label class="switch">
-                <input type="checkbox" onClick={() => this.handleClick(12, "Music")}></input>
-                <span class="slider round"></span>
-              </label>
-            </div>
-            <div className= "griditem">
-              <div className="categoryname">Television</div>
-              <label class="switch">
-                <input type="checkbox" onClick={() => this.handleClick(14, "Television")}></input>
-                <span class="slider round"></span>
-              </label>
-            </div>
-          </div>
-          <div id="column">
-            <div className= "griditem">
-              <div className="categoryname">Video Games</div>
-              <label class="switch">
-                <input type="checkbox" onClick={() => this.handleClick(15, "Video Games")}></input>
-                <span class="slider round"></span>
-              </label>
-            </div>
-            <div className= "griditem">
-              <div className="categoryname">Books</div>
-              <label class="switch">
-                <input type="checkbox" onClick={() => this.handleClick(10, "Books")}></input>
-                <span class="slider round"></span>
-              </label>
-            </div>
-            <div className= "griditem">
-              <div className="categoryname">Anime and Manga</div>
-              <label class="switch">
-                <input type="checkbox" onClick={() => this.handleClick(31, "Anime and Manga")}></input>
-                <span class="slider round"></span>
-              </label>
-            </div>
-            <div className= "griditem">
-              <div className="categoryname">Science and Nature</div>
-              <label class="switch">
-                <input type="checkbox" onClick={() => this.handleClick(17, "Science and Nature")}></input>
-                <span class="slider round"></span>
-              </label>
-            </div>
-          </div>
-          <div id="column">
-            <div className= "griditem">
-              <div className="categoryname">Computers</div>
-              <label class="switch">
-                <input type="checkbox" onClick={() => this.handleClick(18, "Computers")}></input>
-                <span class="slider round"></span>
-              </label>
-            </div>
-            <div className= "griditem">
-              <div className="categoryname">Mathematics</div>
-              <label class="switch">
-                <input type="checkbox" onClick={() => this.handleClick(19, "Mathematics")}></input>
-                <span class="slider round"></span>
-              </label>
-            </div>
-            <div className= "griditem">
-              <div className="categoryname">Mythology</div>
-              <label class="switch">
-                <input type="checkbox" onClick={() => this.handleClick(20, "Mythology")}></input>
-                <span class="slider round"></span>
-              </label>
-            </div>
-            <div className= "griditem">
-              <div className="categoryname">Geography</div>
-              <label class="switch">
-                <input type="checkbox" onClick={() => this.handleClick(22, "Geography")}></input>
-                <span class="slider round"></span>
-              </label>
-            </div>
-          </div>
-          <div id= "column">
-            <div className= "griditem">
-              <div className="categoryname">{"History"}</div>
-              <label class="switch">
-                <input type="checkbox" onClick={() => this.handleClick(23, "History")}></input>
-                <span class="slider round"></span>
-              </label>
-            </div>
-            <div className= "griditem">
-              <div className="categoryname">Politics</div>
-              <label class="switch">
-                <input type="checkbox" onClick={() => this.handleClick(24, "Politics")}></input>
-                <span class="slider round"></span>
-              </label>
-            </div>
-            <div className= "griditem">
-              <div className="categoryname">Animals</div>
-              <label class="switch">
-                <input type="checkbox" onClick={() => this.handleClick(27, "Animals")}></input>
-                <span class="slider round"></span>
-              </label>
-            </div>
-            <div className= "griditem">
-              <div className="categoryname">Celebrities</div>
-              <label class="switch">
-                <input type="checkbox" onClick={() => this.handleClick(26, "Celebrities")}></input>
-                <span class="slider round"></span>
-              </label>
-            </div>
-          </div>
+          )}
         </Grid>
       </div>
     );
