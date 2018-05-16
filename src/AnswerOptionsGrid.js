1db56e735615a220b82684367954a7c476423a02
Emil Jonsson <emijonss@kth.se>
2018-05-16 19:41:26 +0200

    Workin on spectators

--- src/AnswerOptionsGrid.js
+++ src/AnswerOptionsGrid.js
@@ -47,25 +47,51 @@ class AnswerOptionsGrid extends Component {
           <div>
             <Grid className="AlternativesGrid" width={40} gap={30}>
               <div id="column">
-              { this.state.teamChoice == 0 ?
-                <div className="QuestionAlternatives2">
-                  <div>{"Picked by " + this.state.pickedBy}</div>
-                  <button type="button" class="btn btn-primary btn-lg" onClick={() => this.pickAnswer(0)}>{this.state.answerOptions[0].replace(/&quot;/g,"\"").replace(/&#039;/g, "'")}</button>
-                </div> :
-                <div className="QuestionAlternatives">
-                  <button type="button" class="btn btn-primary btn-lg" onClick={() => this.pickAnswer(0)}>{this.state.answerOptions[0].replace(/&quot;/g,"\"").replace(/&#039;/g, "'")}</button>
+                <div>
+                  { this.state.teamChoice == 0 ?
+                    <div className="QuestionAlternatives2">
+                      <div>{"Picked by " + this.state.pickedBy}</div>
+                      <button type="button" class="btn btn-primary btn-lg" onClick={() => this.pickAnswer(0)}>{this.state.answerOptions[0].replace(/&quot;/g,"\"").replace(/&#039;/g, "'")}</button>
+                    </div> :
+                    <div className="QuestionAlternatives">
+                      <button type="button" class="btn btn-primary btn-lg" onClick={() => this.pickAnswer(0)}>{this.state.answerOptions[0].replace(/&quot;/g,"\"").replace(/&#039;/g, "'")}</button>
+                    </div>
+                  }
                 </div>
-              }
-                <div className="QuestionAlternatives">
-                  <button type="button" class="btn btn-primary btn-lg" onClick={() => this.pickAnswer(1)}>{this.state.answerOptions[1].replace(/&quot;/g,"\"").replace(/&#039;/g, "'")}</button>
+                <div>
+                { this.state.teamChoice == 1 ?
+                  <div className="QuestionAlternatives2">
+                    <div>{"Picked by " + this.state.pickedBy}</div>
+                    <button type="button" class="btn btn-primary btn-lg" onClick={() => this.pickAnswer(1)}>{this.state.answerOptions[1].replace(/&quot;/g,"\"").replace(/&#039;/g, "'")}</button>
+                  </div> :
+                  <div className="QuestionAlternatives">
+                    <button type="button" class="btn btn-primary btn-lg" onClick={() => this.pickAnswer(1)}>{this.state.answerOptions[1].replace(/&quot;/g,"\"").replace(/&#039;/g, "'")}</button>
+                  </div>
+                }
                 </div>
               </div>
               <div id="column">
-                <div className="QuestionAlternatives">
-                  <button type="button" class="btn btn-primary btn-lg" onClick={() => this.pickAnswer(2)}>{this.state.answerOptions[2].replace(/&quot;/g,"\"").replace(/&#039;/g, "'")}</button>
+                <div>
+                  { this.state.teamChoice == 2 ?
+                    <div className="QuestionAlternatives2">
+                      <div>{"Picked by " + this.state.pickedBy}</div>
+                      <button type="button" class="btn btn-primary btn-lg" onClick={() => this.pickAnswer(2)}>{this.state.answerOptions[2].replace(/&quot;/g,"\"").replace(/&#039;/g, "'")}</button>
+                    </div> :
+                    <div className="QuestionAlternatives">
+                      <button type="button" class="btn btn-primary btn-lg" onClick={() => this.pickAnswer(2)}>{this.state.answerOptions[2].replace(/&quot;/g,"\"").replace(/&#039;/g, "'")}</button>
+                    </div>
+                  }
                 </div>
-                <div className="QuestionAlternatives">
-                  <button type="button" class="btn btn-primary btn-lg" onClick={() => this.pickAnswer(3)}>{this.state.answerOptions[3].replace(/&quot;/g,"\"").replace(/&#039;/g, "'")}</button>
+                <div>
+                  { this.state.teamChoice == 3 ?
+                    <div className="QuestionAlternatives2">
+                      <div>{"Picked by " + this.state.pickedBy}</div>
+                      <button type="button" class="btn btn-primary btn-lg" onClick={() => this.pickAnswer(3)}>{this.state.answerOptions[3].replace(/&quot;/g,"\"").replace(/&#039;/g, "'")}</button>
+                    </div> :
+                    <div className="QuestionAlternatives">
+                      <button type="button" class="btn btn-primary btn-lg" onClick={() => this.pickAnswer(3)}>{this.state.answerOptions[3].replace(/&quot;/g,"\"").replace(/&#039;/g, "'")}</button>
+                    </div>
+                  }
                 </div>
               </div>
             </Grid>
